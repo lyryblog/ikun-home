62117926042ae27870bb7bca3aa43c5e9a2e81fd
RuoYu <2803138594@qq.com>
2022-12-26 14:47:53 +0800

    Update
    Nothing

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var audio = document.getElementById('audio');
+audio.play();
+var lis = document.querySelectorAll('ul.jj li');
+lis.forEach(li => {
+    li.onclick = function() {
+        audio.src = 'http://jilehe.125ks.cn/Voice/jlh/res/' + this.getAttribute('m') + '.mp3';
+        audio.play();
+        console.log(this.innerHTML)
+    }
+});
+document.querySelector('.s1').onclick = function() {
+    audio.currentTime = 0;
+    console.log('重开')
+};
+document.querySelector('.s2').onclick = function() {
+    audio.pause();
+    console.log('停止')
+}
